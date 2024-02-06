414ad87ea802bf43cbc27116e9cce714fcd92ac7
Yulia <yuliakiss369@gmail.com>
2024-02-06 20:50:40 +0200

    change size config

--- src/Elements/Options/OptionsSizePhoto.js
+++ src/Elements/Options/OptionsSizePhoto.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 function OptionSizePhoto(image,setWidth,setHeight,setSizePhoto,setSizeStyle,setConfigXY){
-   if(image.naturalWidth>image.naturalHeight){
+
        let heightStyle=500
        let widthStyle=500/(image.naturalHeight/image.naturalWidth)
        let height=image.naturalHeight
@@ -14,20 +14,7 @@ function OptionSizePhoto(image,setWidth,setHeight,setSizePhoto,setSizeStyle,setC
            width: `${widthStyle}px`,
            height:`${heightStyle}px`
        })
-   }else{
-       let heightStyle=500/(image.naturalWidth/image.naturalHeight)
-       let widthStyle=500
-       let height=image.naturalHeight
-       let width=image.naturalWidth
-       setHeight(height)
-       setWidth(width)
-       setSizePhoto([width,height])
-       setConfigXY([widthStyle,heightStyle])
-       setSizeStyle({
-           width: `${widthStyle}px`,
-           height:`${heightStyle}px`
-       })
-   }
+
 }
 
 export default OptionSizePhoto
